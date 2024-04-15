14058471ab08111c22b0f116d828485cb1c1bce3
ivanov9826 <ivanov9826@gmail.com>
2024-04-15 22:11:36 +0300

    Replace useEffect with useMemo , fix paragraph width

--- src/components/organisms/PartnerList/PartnerList.tsx
+++ src/components/organisms/PartnerList/PartnerList.tsx
@@ -1,5 +1,5 @@
 import "./PartnerList.scss";
-import { useState, useEffect } from "react";
+import { useMemo } from "react";
 import { eligibleParnter } from "../../../interfaces/interfaces";
 import partnersData from "../../../constants/partnersData.json";
 import rangeCheck from "../../../utils/rangeCheck";
@@ -7,23 +7,20 @@ import PartnerCard from "../../molecules/PartnerCard/PartnerCard";
 import ListTitle from "../../atoms/ListTitle/ListTitle";
 
 const PartnerList: React.FC = () => {
-  const [eligiblePartners, seteligiblePartners] = useState<eligibleParnter[]>([]);
-
-  useEffect(() => {
-    let tempArr: eligibleParnter[] = [];
+  const partnersArray = useMemo(() => {
+    const tempArr: eligibleParnter[] = [];
     partnersData.forEach((partner) => {
       const result = rangeCheck(partner, 100);
       if (result) tempArr.push(result);
     });
-    tempArr = tempArr.sort((a, b) => a.partner_id - b.partner_id);
-    seteligiblePartners(tempArr);
+    return tempArr.sort((a, b) => a.partner_id - b.partner_id);
   }, []);
 
   return (
     <div className="list-wrapper">
       <ListTitle text="Eligible partners:" />
       <div className="partners-container">
-        {eligiblePartners.map((partner, index) => (
+        {partnersArray.map((partner, index) => (
           <PartnerCard partner={partner} key={index} />
         ))}
       </div>
